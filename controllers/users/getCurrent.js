0c69943bc8cebadfda020d88e12366a1cc3e3591
Vlad Makhnenko <vlmakh@gmail.com>
2023-04-05 19:44:01 +0300

    Update getCurrent.js

--- controllers/users/getCurrent.js
+++ controllers/users/getCurrent.js
@@ -1,8 +1,8 @@
 const getCurrent = async (req, res, next) => {
   try {
-    const { name, email, subscription, _id } = await req.user;
+    const { email, subscription, _id } = await req.user;
 
-    res.status(200).json({ name, email, subscription, _id });
+    res.status(200).json({ email, subscription, _id });
   } catch (error) {
     next(error);
   }
