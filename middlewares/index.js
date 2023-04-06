7207f303a237df2fb5422ed3cdc77a5c0130e4ca
Vlad Makhnenko <vlmakh@gmail.com>
2023-04-06 15:41:04 +0300

    add isValidOwner

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,9 @@
 const auth = require("../middlewares/auth");
 const isValidId = require("../middlewares/isValidId");
+const isValidOwner = require("../middlewares/isValidOwner");
 
 module.exports = {
   auth,
   isValidId,
+  isValidOwner,
 };
--- routes/api-contacts.js
+++ routes/api-contacts.js
@@ -1,19 +1,25 @@
 const express = require("express");
 const { contacts: ctrl } = require("../controllers");
-const { auth, isValidId } = require("../middlewares");
+const { auth, isValidId, isValidOwner } = require("../middlewares");
 
 const router = express.Router();
 
 router.get("/", auth, ctrl.listContacts);
 
-router.get("/:contactId", auth, isValidId, ctrl.getContactById);
+router.get("/:contactId", auth, isValidId, isValidOwner, ctrl.getContactById);
 
 router.post("/", auth, ctrl.addContact);
 
-router.delete("/:contactId", auth, isValidId, ctrl.removeContact);
+router.delete("/:contactId", auth, isValidId, isValidOwner, ctrl.removeContact);
 
-router.put("/:contactId", auth, isValidId, ctrl.updateContact);
+router.put("/:contactId", auth, isValidId, isValidOwner, ctrl.updateContact);
 
-router.patch("/:contactId/favorite", auth, isValidId, ctrl.updateFavorite);
+router.patch(
+  "/:contactId/favorite",
+  auth,
+  isValidId,
+  isValidOwner,
+  ctrl.updateFavorite
+);
 
 module.exports = router;
