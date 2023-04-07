5285d3957404fc7ca1a4ca0dae26fa67a51b621d
Vlad Makhnenko <vlmakh@gmail.com>
2023-04-07 13:50:04 +0300

    add listParams name, fav

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -2,15 +2,29 @@ const { Contact } = require("../../models/contact");
 
 const listContacts = async (req, res, next) => {
   const { _id } = await req.user;
-  const { favorite } = req.query;
-  const contact = !favorite
-    ? { owner: _id }
-    : { owner: _id, favorite: favorite };
-  const { page = 1, limit = 3 } = req.query;
+  const { favorite = null, name = null, phone = null } = req.query;
+
+  let listParams = { owner: _id };
+
+  if (favorite) {
+    listParams = { ...listParams, favorite };
+  }
+
+  if (name) {
+    listParams = { ...listParams, name: { $regex: name, $options: "i" } };
+  }
+
+  if (phone) {
+    listParams = { ...listParams, phone };
+  }
+
+  console.log(listParams);
+
+  const { page = 1, limit = 5 } = req.query;
   const skip = (page - 1) * limit;
 
   try {
-    const data = await Contact.find(contact, "-createdAt -updatedAt", {
+    const data = await Contact.find(listParams, "-createdAt -updatedAt", {
       skip,
       limit,
     }).populate("owner", "_id");
