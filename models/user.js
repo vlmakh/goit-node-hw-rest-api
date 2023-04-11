2ee1ab0db51087efb3ac74be9cc1c0dfccd4ff36
Vlad Makhnenko <vlmakh@gmail.com>
2023-04-11 08:48:24 +0300

    update user model

--- models/user.js
+++ models/user.js
@@ -32,6 +32,14 @@ const userSchema = Schema(
       type: String,
       default: null,
     },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+      required: [true, "Verify token is required"],
+    },
   },
   { versionKey: false, timestamps: true }
 );
